0f8d579acadf11ad1222bc991ce7469ff1c86c8c
KKANGDDOL <knemo333@gmail.com>
2022-08-25 21:43:25 +0900

    10시 빌드 파일

---
+++ Assets/02.Manager/MaskManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaskManager : MonoBehaviour
+{
+    public GameObject maskPrefab;
+    public Transform[] fireTr;
+    public float fireDelay;
+    public float maskSpeed;
+
+    
+
+}
